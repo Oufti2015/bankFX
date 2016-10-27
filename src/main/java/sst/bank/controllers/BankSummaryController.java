4190f36192ac57226c773a2d40110ad1c4e8758d
Stéphane Stiennon <stephane.stiennon@gmail.com>
2016-10-27 23:39:04 +0200

    Add summary Table View
    Summary Table View is now populated

--- src/main/java/sst/bank/controllers/BankSummaryController.java
+++ src/main/java/sst/bank/controllers/BankSummaryController.java
@@ -1,6 +1,8 @@
 package sst.bank.controllers;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -9,9 +11,13 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lombok.extern.log4j.Log4j;
 import sst.bank.model.BankSummary;
+import sst.bank.model.Category;
+import sst.bank.model.SummaryModel;
 
 @Log4j
 public class BankSummaryController {
@@ -25,18 +31,51 @@ public class BankSummaryController {
     ListView<BankSummary> listViewByMonth;
     @FXML
     OperationsListController operationsByMonthController;
+    @FXML
+    TableView<SummaryModel> budgetTableView;
+    @FXML
+    private TableColumn<SummaryModel, String> sumCategoryColumn;
+    @FXML
+    private TableColumn<SummaryModel, Double> sumAmountColumn;
+    @FXML
+    private TableColumn<SummaryModel, Double> sumBudgetColumn;
+    @FXML
+    private TableColumn<SummaryModel, Double> sumDiffColumn;
 
     @FXML
     private void initialize() {
 	log.info("initialize...");
-	
+	if (budgetTableView == null) {
+	    log.fatal("budgetTableView is not initialised...");
+	    System.exit(-1);
+	}
+	if (sumCategoryColumn == null) {
+	    log.fatal("sumCategoryColumn is not initialised...");
+	    System.exit(-1);
+	}
+	sumCategoryColumn.setCellValueFactory(new PropertyValueFactory<SummaryModel, String>("category"));
+	sumAmountColumn.setCellValueFactory(new PropertyValueFactory<SummaryModel, Double>("amount"));
+	sumBudgetColumn.setCellValueFactory(new PropertyValueFactory<SummaryModel, Double>("budget"));
+	sumDiffColumn.setCellValueFactory(new PropertyValueFactory<SummaryModel, Double>("diff"));
+
 	listViewByMonth.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<BankSummary>() {
 
 	    @Override
-	    public void changed(ObservableValue<? extends BankSummary> observable, BankSummary oldValue, BankSummary newValue) {
-	        // Your action here
-	       log.debug("Selected item: " + newValue);
-	       operationsByMonthController.setData(newValue);
+	    public void changed(ObservableValue<? extends BankSummary> observable, BankSummary oldValue,
+				BankSummary newValue) {
+		// Your action here
+		log.debug("Selected item: " + newValue);
+		operationsByMonthController.setData(newValue);
+		fromLabel.setText("From " + newValue.getStartDate() + " to " + newValue.getEndDate());
+
+		List<SummaryModel> list = new ArrayList<>();
+		for (Category cat : newValue.getSummary().keySet()) {
+		    SummaryModel e = new SummaryModel(cat, newValue.getSummary().get(cat).doubleValue());
+		    list.add(e);
+		    log.debug("" + cat + " : " + e);
+		}
+
+		budgetTableView.getItems().setAll(list);
 	    }
 	});
     }
@@ -51,15 +90,5 @@ public class BankSummaryController {
 	    items.add(item);
 	}
 	listViewByMonth.setItems(items);
-	listViewByMonth.setEditable(true);
-	listViewByMonth.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-	listViewByMonth.getSelectionModel().selectedItemProperty().addListener(
-		new ChangeListener<BankSummary>() {
-		    @Override
-		    public void changed(ObservableValue<? extends BankSummary> ov,
-					BankSummary old_val, BankSummary new_val) {
-			log.debug("" + old_val + "-" + new_val);
-		    }
-		});
     }
 }
---
+++ src/main/java/sst/bank/model/SummaryModel.java
@@ -0,0 +1,58 @@
+package sst.bank.model;
+
+import java.math.BigDecimal;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class SummaryModel {
+    private StringProperty category;
+    private DoubleProperty amount;
+    private DoubleProperty budget;
+    private DoubleProperty diff;
+
+    public SummaryModel(Category category, double amount) {
+
+	this.category = new SimpleStringProperty(category.getName());
+	this.amount = new SimpleDoubleProperty(amount);
+	BigDecimal budgetMonthlyControlledAmount = category.getBudget().monthlyControlledAmount();
+	this.budget = new SimpleDoubleProperty(budgetMonthlyControlledAmount.doubleValue());
+
+	BigDecimal subtractDiff = BigDecimal.valueOf(amount).subtract(budgetMonthlyControlledAmount);
+	this.diff = new SimpleDoubleProperty(subtractDiff.doubleValue());
+    }
+
+    public StringProperty category() {
+	return category;
+    }
+
+    public DoubleProperty amount() {
+	return amount;
+    }
+
+    public DoubleProperty budget() {
+	return budget;
+    }
+
+    public DoubleProperty diff() {
+	return diff;
+    }
+
+    public String getCategory() {
+	return category.get();
+    }
+
+    public Double getAmount() {
+	return amount.get();
+    }
+
+    public Double getBudget() {
+	return budget.get();
+    }
+
+    public Double getDiff() {
+	return diff.get();
+    }
+}
