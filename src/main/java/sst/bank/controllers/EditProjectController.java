40e7abb21d2c577206d3c670a39a15e0f9310c8b
Stéphane Stiennon <stephane.stiennon@gmail.com>
2017-06-20 12:33:03 +0200

    Projects

---
+++ src/main/java/sst/bank/controllers/EditProjectController.java
@@ -0,0 +1,93 @@
+package sst.bank.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import lombok.extern.log4j.Log4j;
+import sst.bank.OuftiBankFX;
+import sst.bank.events.ProjectChangeEvent;
+import sst.bank.model.BankSummary;
+import sst.bank.model.Operation;
+import sst.bank.model.Project;
+
+@Log4j
+public class EditProjectController {
+    private Project project;
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private DatePicker startDatePicker;
+    @FXML
+    private DatePicker endDatePicker;
+    @FXML
+    private OperationsListController operationsByProjectController;
+    @FXML
+    private Button okButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void initialize() {
+	log.debug("initialize...");
+
+	setEditable(false);
+
+	okButton.setOnAction(new EventHandler<ActionEvent>() {
+
+	    @Override
+	    public void handle(ActionEvent e) {
+		project.setName(nameTextField.getText());
+		project.setId(idTextField.getText());
+		project.setStartDate(startDatePicker.getValue());
+		project.setEndDate(endDatePicker.getValue());
+
+		OuftiBankFX.eventBus.post(new ProjectChangeEvent(project));
+		setEditable(false);
+	    }
+	});
+
+	cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+	    @Override
+	    public void handle(ActionEvent e) {
+		setProjectInfo();
+	    }
+	});
+    }
+
+    private void setEditable(boolean b) {
+	nameTextField.setDisable(!b);
+	idTextField.setDisable(!b);
+	okButton.setDisable(!b);
+	cancelButton.setDisable(!b);
+    }
+
+    public void setData(Project project) {
+	this.project = project;
+
+	if (project != null) {
+	    setProjectInfo();
+	    setEditable(true);
+	}
+    }
+
+    private void setProjectInfo() {
+	nameTextField.setText(project.getName());
+	idTextField.setText(project.getId());
+	startDatePicker.setValue(project.getStartDate());
+	endDatePicker.setValue(project.getEndDate());
+
+	ObservableList<Operation> operations = FXCollections.observableArrayList();
+	for (Operation operation : project.getOperations()) {
+	    operations.add(operation);
+	}
+
+	operationsByProjectController.setData(new BankSummary(operations));
+    }
+}
--- src/main/java/sst/bank/controllers/ProjectsController.java
+++ src/main/java/sst/bank/controllers/ProjectsController.java
@@ -1,7 +1,12 @@
 package sst.bank.controllers;
 
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+
 import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.Subscribe;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -9,62 +14,59 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import lombok.extern.log4j.Log4j;
-import org.junit.Assert;
 import sst.bank.OuftiBankFX;
-import sst.bank.events.BeneficiaryChangeEvent;
+import sst.bank.events.ProjectChangeEvent;
 import sst.bank.model.Project;
 import sst.bank.model.container.BankContainer;
-
-import java.util.stream.Collectors;
 
 @Log4j
 public class ProjectsController {
     @FXML
     private ListView<Project> projectsListView;
     @FXML
-    private EditBeneficiaryController editProjectController;
+    private EditProjectController editProjectController;
 
     @FXML
     private void initialize() {
-        log.debug("initialize...");
+	log.debug("initialize...");
 
-        OuftiBankFX.eventBus.register(this);
+	OuftiBankFX.eventBus.register(this);
 
-        Assert.assertNotNull(projectsListView);
-        Assert.assertNotNull(editProjectController);
-        ObservableList<Project> projects = FXCollections.observableArrayList();
-        for (Project project : BankContainer.me().projectsContainer().projects()
-                .stream()
-                .sorted()
-                .collect(Collectors.toList())) {
-            projects.add(project);
-        }
+	Assert.assertNotNull(projectsListView);
+	Assert.assertNotNull(editProjectController);
+	ObservableList<Project> projects = FXCollections.observableArrayList();
+	for (Project project : BankContainer.me().projectsContainer().projects()
+		.stream()
+		.sorted()
+		.collect(Collectors.toList())) {
+	    projects.add(project);
+	}
 
-        projectsListView.getItems().setAll(projects);
-        projectsListView.getSelectionModel().selectedItemProperty().addListener(
-                new ChangeListener<Project>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Project> ov,
-                                        Project old_val, Project new_val) {
-                        //editBeneficiaryController.setData(new_val);
-                    }
-                });
+	projectsListView.getItems().setAll(projects);
+	projectsListView.getSelectionModel().selectedItemProperty().addListener(
+		new ChangeListener<Project>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Project> ov,
+					Project old_val, Project new_val) {
+			editProjectController.setData(new_val);
+		    }
+		});
     }
 
     @Subscribe
-    public void handleEvent(BeneficiaryChangeEvent e) {
-        ObservableList<Project> projects = FXCollections.observableArrayList();
-        for (Project project : BankContainer.me().projectsContainer().projects()
-                .stream()
-                .sorted()
-                .collect(Collectors.toList())) {
-            projects.add(project);
-        }
-        projectsListView.getItems().setAll(projects);
+    public void handleEvent(ProjectChangeEvent e) {
+	ObservableList<Project> projects = FXCollections.observableArrayList();
+	for (Project project : BankContainer.me().projectsContainer().projects()
+		.stream()
+		.sorted()
+		.collect(Collectors.toList())) {
+	    projects.add(project);
+	}
+	projectsListView.getItems().setAll(projects);
     }
 
     @Subscribe
     public void deadEvents(DeadEvent e) {
-        log.debug("Event " + e.getEvent() + " not subscribed...");
+	log.debug("Event " + e.getEvent() + " not subscribed...");
     }
 }
